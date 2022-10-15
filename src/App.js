2b9909bd40fd7f8987c347c976da738f313be65f
Dharmendra Singh <dharmendra.rex@gmail.com>
2022-10-16 02:01:08 +0530

    Simple users using fetch api and searching the state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component() {
+class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+
+    console.log('constructor');
+  }
+
+  //Lifecycle method
+  componentDidMount() {
+    console.log('componentDidMount');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            //console.log(this.state);
+          }
+        );
+      });
+  }
+
+  onSearchInput = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  }
 
   render() {
+    console.log('render');
+
+    const filterMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
-        <h1>Dharmendra</h1>
+
+        <input type='search' className='search-box' placeholder='Search Monsters' onChange={this.onSearchInput} />
+
+        {filterMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
